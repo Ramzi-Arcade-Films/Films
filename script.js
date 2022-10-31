320bfd6697b50babd00fb3f6b578877c7ef2c96f
DMarques Coleman <dmcolem2@asu.edu>
2022-10-31 13:13:18 -0500

    pages
    html css java added

---
+++ script.js
@@ -0,0 +1,12 @@
+const showBtn = document.querySelector(".navBtn");
+const topNav = document.querySelector(".top-nav");
+
+showBtn.addEventListener("click", function(){
+    if(topNav.classList.contains("showNav")){
+    topNav.classList.remove("showNav");
+    showBtn.innerHTML = '<i class = "fas fa-times"></i>';
+} else {
+    topNav.classList.add("showNav");
+    showBtn.innerHTML = '<i class ="fas fa-times"></i>';
+}
+});
